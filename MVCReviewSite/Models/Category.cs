9c8d765b8b3f23197a3143a1e9ae4dff4e61b378
mikespriggs26 <mikespriggs@msn.com>
2017-10-20 11:57:50 -0400

    First commit

---
+++ MVCReviewSite/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCReviewSite.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }        
+
+    }
+}
---
+++ MVCReviewSite/Models/Review.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Name")]
+        public string UserName { get; set; }
+        public string Location { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
+        public DateTime PublishedDate { get; set; }
+        public string Content { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
